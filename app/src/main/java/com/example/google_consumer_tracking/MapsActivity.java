6aca36fe4b650023e42af4fe88d61f95442d1e00
Sergey Bryukhanov <sergey.brukhanov@xpress.com.ph>
2023-02-09 16:49:37 +0700

    setup

--- app/src/main/java/com/example/google_consumer_tracking/MapsActivity.java
+++ app/src/main/java/com/example/google_consumer_tracking/MapsActivity.java
@@ -2,27 +2,27 @@ package com.example.google_consumer_tracking;
 
 import static java.util.Objects.requireNonNull;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.FragmentActivity;
 
-import android.annotation.SuppressLint;
-import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.google_consumer_tracking.databinding.ActivityMapsBinding;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.tasks.Task;
 import com.google.android.libraries.mapsplatform.transportation.consumer.ConsumerApi;
+import com.google.android.libraries.mapsplatform.transportation.consumer.managers.TripModel;
+import com.google.android.libraries.mapsplatform.transportation.consumer.managers.TripModelCallback;
 import com.google.android.libraries.mapsplatform.transportation.consumer.managers.TripModelManager;
+import com.google.android.libraries.mapsplatform.transportation.consumer.model.TripInfo;
+import com.google.android.libraries.mapsplatform.transportation.consumer.model.TripModelOptions;
+import com.google.android.libraries.mapsplatform.transportation.consumer.sessions.JourneySharingSession;
 import com.google.android.libraries.mapsplatform.transportation.consumer.view.ConsumerController;
 import com.google.android.libraries.mapsplatform.transportation.consumer.view.ConsumerGoogleMap;
 import com.google.android.libraries.mapsplatform.transportation.consumer.view.ConsumerMapView;
@@ -47,6 +47,9 @@ public class MapsActivity extends FragmentActivity {
     private ConsumerApi consumerApi;
     @MonotonicNonNull private TripModelManager tripModelManager;
     private ConsumerGoogleMap consumerGoogleMap;
+    private JourneySharingSession session;
+    private Button actionButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,18 +57,24 @@ public class MapsActivity extends FragmentActivity {
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-
+        consumerMapView = binding.consumerMapView;
+        actionButton = findViewById(R.id.buttonCreateTrip);
+        actionButton.setOnClickListener(this::onActionButtonClicked);
 
         initializeSdk();
+        getComsumerApi();
+        initConsumerController();
         Log.i(TAG, "Consumer SDK version: " + ConsumerApi.getConsumerSDKVersion());
         Toast.makeText(this, "Consumer SDK version: " + ConsumerApi.getConsumerSDKVersion(), Toast.LENGTH_LONG).show();
-
-
     }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
 
+        if (session != null) {
+            session.stop();
+        }
+    }
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -86,27 +95,71 @@ public class MapsActivity extends FragmentActivity {
                         // Safe to do so as controller will only be nullified during consumerMap's onDestroy()
                         consumerGoogleMap = consumerMap;
                         consumerController = consumerMap.getConsumerController();
-                        Task<ConsumerApi> consumerApiTask =
-                                ConsumerApi.initialize(
-                                        MapsActivity.this,
-                                        "xpress-366609",
-                                        new JsonAuthTokenFactory());
-                        consumerApiTask.addOnSuccessListener(
-                                consumerApi ->
-                                        tripModelManager = requireNonNull(consumerApi.getTripModelManager()));
-                        consumerApiTask.addOnFailureListener(
-                                task -> Log.e(TAG, "ConsumerApi Initialization Error:\n" + task.getMessage()));
-//                        ConsumerMarkerUtils.setCustomMarkers(consumerController, SampleAppActivity.this);
-//                        setupViewBindings();
-                        googleMap = consumerGoogleMap;
-//                        centerCameraToLastLocation();
-//                        setupMapListener();
                     }
                 },
                 /* fragmentActivity= */ this,
                 /* googleMapOptions= */ null);
     }
 
+    private void getComsumerApi(){
+        Task<ConsumerApi> consumerApiTask = ConsumerApi.initialize(
+                this, "xpress-366609", new JsonAuthTokenFactory());
+
+        consumerApiTask.addOnSuccessListener(
+                consumerApiSdk -> this.consumerApi = consumerApiSdk);
+        consumerApiTask.addOnFailureListener(
+                task -> Log.e(TAG, "ConsumerApi Initialization Error:\n" + task.getMessage()));
+    }
+
+    private void initConsumerController(){
+        consumerMapView.getConsumerGoogleMapAsync(
+                new ConsumerGoogleMap.ConsumerMapReadyCallback() {
+                    @Override
+                    public void onConsumerMapReady(@NonNull ConsumerGoogleMap consumerMap) {
+                        consumerGoogleMap = consumerMap;
+                        consumerController = consumerMap.getConsumerController();
+                    }
+                },
+                this, null);
+    }
+
+    private void createTrip(){
+        String tripName = "TestDrive";
+        tripModelManager = consumerApi.getTripModelManager();
+        TripModel tripModel = tripModelManager.getTripModel(tripName);
+
+// Create a JourneySharingSession instance based on the TripModel.
+        session = JourneySharingSession.createInstance(tripModel);
+
+// Add the JourneySharingSession instance on the map for updating the UI.
+        consumerController.showSession(session);
+        consumerController.setAutoCameraEnabled(true);
+// Register for trip update events.
+        tripModel.registerTripCallback(new TripModelCallback() {
+            @Override
+            public void onTripETAToNextWaypointUpdated(
+                    TripInfo tripInfo, @Nullable Long timestampMillis) {
+                // ...
+            }
+
+            @Override
+            public void onTripActiveRouteRemainingDistanceUpdated(
+                    TripInfo tripInfo, @Nullable Integer distanceMeters) {
+                // ...
+            }
+
+            // ...
+        });
+
+        // Set refresh interval to 2 seconds.
+        TripModelOptions tripOptions =
+                TripModelOptions.builder().setRefreshIntervalMillis(2000).build();
+        tripModel.setTripModelOptions(tripOptions);
+    }
+    private void onActionButtonClicked(View view) {
+        Toast.makeText(this, "But Click", Toast.LENGTH_LONG).show();
+        createTrip();
+    }
 
 
 }
